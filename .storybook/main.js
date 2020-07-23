f1ba7158a248099ae8c251b487dd95aade8fa448
Min Kim <min.kim@tabcorp.com.au>
2020-07-23 14:25:28 +1000

    update storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,16 +3,13 @@ module.exports = {
   webpackFinal: async config => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve('ts-loader'),
-        },
-        {
-          loader: require.resolve('react-docgen-typescript-loader'),
-        },
-      ],
+      loader: require.resolve('babel-loader'),
+      options: {
+        presets: [['react-app', { flow: false, typescript: true }]],
+      },
     });
     config.resolve.extensions.push('.ts', '.tsx');
     return config;
   },
+  addons: ['@storybook/preset-create-react-app', '@storybook/addon-actions', '@storybook/addon-links'],
 };
